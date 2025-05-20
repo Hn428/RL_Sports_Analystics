eae0acd4a3ef65ae20e25c67a0df8fc0af1f2afa
hanzalanadeem <hanzalanadeem4@gmail.com>
2025-05-20 15:44:39 -0400

    trained model now awaiting testing

--- train/train_agent.py
+++ train/train_agent.py
@@ -1,26 +1,21 @@
-import sys
-import os
-
-# Add project root to sys.path
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.insert(0, project_root)
-
-from environments.predict_env import NBAPredictEnv  
 import pandas as pd
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
 
-# Load preprocessed game data
-games_df = pd.read_csv('nbaData/PreprocessedGames.csv')
+from environments.predict_env import NBAPredictEnv
 
-# Create and check environment
-env = NBAPredictEnv(games_df)
-check_env(env)
+print("Loading data...")
+games_df = pd.read_csv("nbaData/PreprocessedGames.csv", low_memory=False)
+team_stats_df = pd.read_csv("nbaData/TeamStatistics.csv", low_memory=False)
 
-# Train the model
+print("Creating environment...")
+env = NBAPredictEnv(games_df, team_stats_df)
+check_env(env, warn=True)
+
+print("Training model...")
 model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10000)
+model.learn(total_timesteps=100000)
 
-# Save model
+print("✅ Model trained. Saving...")
 model.save("models/nba_rl_predictor")
 print("✅ Model saved")
